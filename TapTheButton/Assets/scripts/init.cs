28a91495b521f71924ecafab5921fd71d44c56a1
tbll75 <thibault@pistache-app.com>
2015-10-03 07:38:08 +0200

    impact headset

--- TapTheButton/Assets/scripts/init.cs
+++ TapTheButton/Assets/scripts/init.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class init : MonoBehaviour {
 
 	public bool isHeadsetConnected = false;
+	public GameObject helpButton;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,7 @@ public class init : MonoBehaviour {
 		isHeadsetConnected = DetectHeadset.Detect();
 
 		Debug.Log ("Headset : " + isHeadsetConnected);
+		helpButton.SetActive (isHeadsetConnected);
 	}
 	
 	// Update is called once per frame
