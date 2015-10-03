c607074bc29760b074b0ffd6992d0442b967059c
tbll75 <thibault@pistache-app.com>
2015-10-03 07:33:38 +0200

    headset detect

---
+++ TapTheButton/Assets/Plugins/DetectHeadset.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class DetectHeadset {
+
+	[DllImport ("__Internal")]
+	static private extern bool _Detect();
+
+	#if UNITY_ANDROID
+		static AndroidJavaClass androidClass;
+	#endif
+
+	static public bool Detect() {
+
+		#if UNITY_IOS
+			return _Detect();
+
+		#elif UNITY_ANDROID && !UNITY_EDITOR
+			
+			if (androidClass == null) {
+				AndroidJNI.AttachCurrentThread();
+				androidClass = new AndroidJavaClass("com.davikingcode.DetectHeadset.DetectHeadset");
+			}
+
+			return androidClass.CallStatic<bool>("_Detect");
+
+		#else
+			return false;
+		#endif
+	}
+}
---
+++ TapTheButton/Assets/scripts/init.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class init : MonoBehaviour {
+
+	public bool isHeadsetConnected = false;
+
+	// Use this for initialization
+	void Start () {
+	
+		isHeadsetConnected = DetectHeadset.Detect();
+
+		Debug.Log ("Headset : " + isHeadsetConnected);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
