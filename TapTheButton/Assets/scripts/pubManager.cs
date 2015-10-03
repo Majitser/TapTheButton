246e74eeab75b3c60f4448864dbd5af00000a324
tbll75 <thibault@pistache-app.com>
2015-10-03 05:31:22 +0200

    add adds

---
+++ TapTheButton/Assets/scripts/pubManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class pubManager : MonoBehaviour {
+
+	public static pubManager instance = null;
+
+	public GameObject starButton;
+
+	// Use this for initialization
+	void Awake () {
+
+		if (instance == null)
+			instance = this;
+		else
+		{
+			Destroy(this.gameObject);
+			Destroy(this);
+		}
+
+		DontDestroyOnLoad(transform.gameObject);
+
+#if UNITY_ANDROID
+		Advertisement.Initialize ("1005607");
+#elif UNITY_IPHONE
+		Advertisement.Initialize ("1005608");
+#endif
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if(Advertisement.isReady())
+		{ 
+			if (starButton != null && starButton.activeSelf == false)
+				starButton.SetActive(true);
+		}
+	}
+
+	public void showAdd()
+	{
+		Advertisement.Show(); 
+	}
+
+
+}
